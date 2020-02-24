7c73fba4d914292201e8b41a95fb91b890ea7ad4
YaseminLi <yaseminli@163.com>
2020-02-24 23:23:32 +0800

    vuepress搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    base: '/',
+    title: '前端面试题整理',
+    description: '前端面试题整理 ',
+    themeConfig: {
+        // GitHub仓库
+        repo: 'https://github.com/YaseminLi',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        // nav: [
+        //     { text: 'Home', link: '/' },
+        //     { text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+        // ],
+        sidebar: [
+            ['/', '题目汇总'],
+            ['/collection/变量类型与计算.md', '变量类型与计算']
+        ]
+    }
+}
